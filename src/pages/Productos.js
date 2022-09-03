6f29fc3a7b35f06f1f67d9139467bb256000b1bb
estsban11 <estsban11@gmail.com>
2022-09-02 23:08:39 -0500

    rt2

--- src/pages/Productos.js
+++ src/pages/Productos.js
@@ -1,21 +1,36 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate} from "react-router-dom";
 
 export const Productos = () => {
   let { search } = useLocation();
   let quey = new URLSearchParams(search);
-  let start = quey.get("inicio");
-  let end = quey.get("fin");
-  console.log(start + end);
-  
+  let start =  parseInt( quey.get("inicio")) || 1;
+  let end = parseInt(quey.get("fin")) || 20;
+  let history = useNavigate();
+  const LIMIT = 20;
+
+  const handlePrev = (e) => { 
+    history({
+      search: `?inicio=${start - LIMIT}&fin=${end - LIMIT}`,
+    });
+
+  }
+  const handleNext = (e) => {
+    history({
+      search:`?inicio=${start+LIMIT}&fin=${end+LIMIT}`
+    });
+  };
   return (
     <div>
       <h3>Productos</h3>
       <p>
-        Mostrando productos del <b>{start}al { end}</b>
+        Mostrando productos del{" "}
+        <b>
+          {start}al {end}
+        </b>
       </p>
-      <button>Atras</button>
-      <button>Adelante</button>
+      {start > LIMIT && <button onClick={handlePrev}>Atras</button>}
+      <button onClick={handleNext}>Adelante</button>
     </div>
   );
 };
